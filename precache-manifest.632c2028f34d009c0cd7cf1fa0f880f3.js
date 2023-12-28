b53b1ae7bc5a2fa42714e96df9780bae759f8a9a
Asus <mkmani176@gmail.com>
2023-12-28 23:29:17 +0530

    Updates

---
+++ precache-manifest.632c2028f34d009c0cd7cf1fa0f880f3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a701b471652fc6528652b9d9571e8ab3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "282d67fc87d59c922df4",
+    "url": "/portfolio/static/css/main.b1578587.chunk.css"
+  },
+  {
+    "revision": "610e07e0623165ead96b",
+    "url": "/portfolio/static/js/2.cdf0545a.chunk.js"
+  },
+  {
+    "revision": "c7df833156a1790b4f9c0891a7e831c3",
+    "url": "/portfolio/static/js/2.cdf0545a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "282d67fc87d59c922df4",
+    "url": "/portfolio/static/js/main.2143b284.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "c914ddc69042ff6e2751756bce84a911",
+    "url": "/portfolio/static/media/intro-bg.c914ddc6.jpg"
+  },
+  {
+    "revision": "9bc1c9605ea700418223c0695d31a90a",
+    "url": "/portfolio/static/media/project1.9bc1c960.png"
+  },
+  {
+    "revision": "c7dcd124cd89d9b0449a5fdb0cc9397d",
+    "url": "/portfolio/static/media/project2.c7dcd124.png"
+  },
+  {
+    "revision": "04395845d40f913e69098b1e8b969857",
+    "url": "/portfolio/static/media/project3.04395845.png"
+  },
+  {
+    "revision": "3e6636bfff3126c8169e40cc731c5ac4",
+    "url": "/portfolio/static/media/react1.3e6636bf.jpg"
+  },
+  {
+    "revision": "c3b144dcada12acfc7e0d6219f819948",
+    "url": "/portfolio/static/media/react2.c3b144dc.webp"
+  }
+]);
